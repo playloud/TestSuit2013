93f1874656dcc9d3e5795dc778bda32dd503de4b
SePark <SePark@WARR-4MLPFX1>
2016-08-03 16:28:57 -0400

    PSH 08/03/16 16:28:55 :

--- HR_Algo_Impl_CutTheSticks/Solution.cs
+++ HR_Algo_Impl_CutTheSticks/Solution.cs
@@ -8,12 +8,38 @@ namespace HR_Algo_Impl_CutTheSticks
 {
 	class Solution
 	{
+
+		/// <summary>
+		/// sticks-length        length-of-cut   sticks-cut
+		//  5 4 4 2 2 8             2               6
+		//  3 2 2 _ _ 6             2               4
+		//  1 _ _ _ _ 4             1               2
+		//  _ _ _ _ _ 3             3               1
+		//  _ _ _ _ _ _           DONE DONE
+		/// </summary>
+		/// <param name="args"></param>
+
 		static void Main(string[] args)
 		{
 			int n = Convert.ToInt32(Console.ReadLine());
 			string[] arr_temp = Console.ReadLine().Split();
-			int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+			int[] sticks = Array.ConvertAll(arr_temp, Int32.Parse);
 
+
+			while (sticks.Sum() > 0)
+			{
+				int minimum = sticks.Where(a => a > 0).Min();
+				int sticksCutCount = 0;
+
+				for (int i = 0; i < sticks.Length; i++)
+				{
+					if(sticks[i]==0)
+						continue;
+					sticks[i] = sticks[i] - minimum;
+					sticksCutCount++;
+				}
+				Console.WriteLine(sticksCutCount);
+			}
 
 		}
 	}
---
+++ HR_Algo_Impl_NonDivisibleSubset/Soultion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Algo_Impl_NonDivisibleSubset
+{
+	class Soultion
+	{
+		/// <summary>
+		/// // PSH 08/03/16 :
+		/// Not a completed solution, 
+		/// pass 4/15
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+
+			int[] nk = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
+			int n = nk[0];
+			int k = nk[1];
+			int[] elements = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
+
+
+			int[] remainderCounts = new int[k];
+			for (int i = 0; i < elements.Length; i++)
+			{
+				int remainder = elements[i]%k;
+				remainderCounts[remainder]++;
+			}
+
+			int total = 0;
+			for (int i = 1; i < k; i++)
+			{
+				total += Math.Max(remainderCounts[i], remainderCounts[k - i]);
+				
+				if(i == k-i)
+					break;
+
+				if (i+i+1 == k)
+					break;
+			}
+
+			Console.WriteLine(total);
+		}
+	}
+}
