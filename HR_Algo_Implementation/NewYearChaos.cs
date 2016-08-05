70d74208a17523aae97f6c8b4a235e6104e24db6
SePark <SePark@WARR-4MLPFX1>
2016-08-05 16:40:10 -0400

    PSH 08/03/16 16:28:59 :

---
+++ HR_Algo_Implementation/NewYearChaos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Algo_Implementation
+{
+public static class NewYearChaos
+{
+
+	public static void DoJob()
+	{
+		List<PeopleLine> caseList = new List<PeopleLine>();
+		int numberOfCases = int.Parse(Console.ReadLine());
+		for (int i = 0; i < numberOfCases; i++)
+		{
+			PeopleLine line = new PeopleLine();
+			line.NumberOfPeople = int.Parse(Console.ReadLine());
+			line.SetLine(Console.ReadLine());
+			caseList.Add(line);
+		}
+
+		foreach (PeopleLine pl in caseList)
+		{
+			pl.Calculate();
+		}
+	}
+
+}
+
+public class PeopleLine
+{
+	private int[] line = null;
+	public int NumberOfPeople { get; set; }
+
+	public void SetLine(string readLine)
+	{
+		line = readLine.Split().Select(a => int.Parse(a)).ToArray();
+	}
+
+	public void Calculate()
+	{
+		var query = line.Select((a, i) => new
+		{
+			value = a,
+			index = i+1
+		});
+
+		if (query.Any())
+		{
+			int sum = 0;
+
+			foreach (var result in query)
+			{
+				if (result.value - result.index > 2)
+				{
+					Console.WriteLine("Too chaotic");
+					return;
+				} else if (result.value > result.index)
+				{
+					sum += (result.value - result.index);
+				}
+			}
+			Console.WriteLine(sum);
+		}
+
+	}
+}
+
+
+}
---
+++ HR_Algo_Implementation/StrangeCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HR_Algo_Implementation
+{
+	public static class StrangeCounter
+	{
+		public static void DoJob()
+		{
+			PrlongInput(999999766777);
+			
+		}
+
+		public static void PrlongInput(double input)
+		{
+			double startIndex = 1;
+			double endIndex = 0;
+			double length = 0;
+
+			for (int i = 0; i < 1000; i++)
+			{
+				startIndex = endIndex + 1;
+				length = 3 << i;
+				endIndex += length;
+
+				Console.WriteLine(endIndex);
+				
+				if (startIndex <= input && input <= endIndex)
+				{
+					Console.WriteLine(endIndex + 1 - input);
+					return;
+				}
+			}
+		}
+	}
+}
